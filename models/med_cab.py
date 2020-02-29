d5e0460b6345b46c0ed070616c9c2ca56cc53e1f
John Mercedes <mercedes.john@gmail.com>
2020-02-28 23:51:06 -0500

    added initial template

---
+++ models/med_cab.py
@@ -0,0 +1,7 @@
+'''
+The Models folder consists of files representing trained/retrained models as
+part of build jobs, etc. The model names can be appropriately set as
+projectname_date_time or project_build_id (in case the model is created as part
+of build jobs). Another approach is to store the model files in a separate storage
+such as AWS S3, Google Cloud Storage, or any other form of storage.
+'''
