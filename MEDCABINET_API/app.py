06e40047b10b36fb8b2686e7b5e01a5f7cd4e6f2
andronikmk <andronikmk@gmail.com>
2020-03-01 19:57:35 -0500

    Add dummy variables to test search function

--- MEDCABINET_API/app.py
+++ MEDCABINET_API/app.py
@@ -1,5 +1,5 @@
 """Main application and routing logic for MEDCABINET_API"""
-from flask import Flask
+from flask import Flask, jsonify
 from decouple import config
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,9 +12,29 @@ def create_app():
     db =SQLAlchemy(app)
     db.init_app(app)
 
+    test_string = """I have no appetite and want to feel euphoria.
+    Also I suffer from insomnia and like fruity flavors"""
 
-    @app.route("/", methods=['POST', 'GET'])
+    returned_values = [{1: [{'Strain': 'Fruity-Pebbles'},
+                            {'Type': 'hybrid'},
+                            {'Effects': 'Happy,Relaxed,Uplifted,Euphoric,Giggly'},
+                            {'Flavor': 'Sweet,Tropical,Berry'},
+                            {'Description': 'Fruity Pebbles (AKA Fruity Pebbles OG) by Alien Genetics was a limited-time offering from the breeder. This sweet hybrid takes genetics from Green Ribbon, Granddaddy Purple, and Tahoe Alien\xa0to create a tropical, berry flavor reminiscent of the cereal. The euphoric effects will keep you happy when you’re stressed and help you catch some sleep when faced with insomnia. Sit back, relax, and pour yourself a bowl of Fruity Pebbles!'}]}]
+
+
+    @app.route("/")
     def root():
+        """Root page
+        
+        Returns:
+            Should provide a link to base.html
+        """
         return 'Minimum viable version.'
+    
+    @app.route("/test/", methods=['GET', 'POST'])
+    def test_search(user_input=test_string):
+        """Takes the input and predicts set of recommendations"""
+
+        return jsonify(user_input, returned_values)
 
     return app
