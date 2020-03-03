bb0c8686bb173122d2908821429195e8b8d2cc05
andronikmk <andronikmk@gmail.com>
2020-03-02 20:12:57 -0500

    Add Procfile

--- API/app.py
+++ API/app.py
@@ -8,24 +8,24 @@ import psycopg2
 import os
 
 #######################################################################################################################
-""" This section to be cleaned up eventually but put in place to have a working pipeline for now"""
-import pickle
-import pandas as pd
-from sklearn.neighbors import NearestNeighbors
-from sklearn.feature_extraction.text import TfidfVectorizer
+# """ This section to be cleaned up eventually but put in place to have a working pipeline for now"""
+# import pickle
+# import pandas as pd
+# from sklearn.neighbors import NearestNeighbors
+# from sklearn.feature_extraction.text import TfidfVectorizer
 
-model = pickle.load(open("../models/nearest_neighbors_model.sav", "rb"))
-transformer = pickle.load(open("../models/transformer.sav", "rb"))
-strains = pd.read_csv("../src/data/nn_model_strains.csv")
+# model = pickle.load(open("../models/nearest_neighbors_model.sav", "rb"))
+# transformer = pickle.load(open("../models/transformer.sav", "rb"))
+# strains = pd.read_csv("../src/data/nn_model_strains.csv")
 
 
-def predict(request_text):
-    transformed = transformer.transform(request_text)
-    dense = transformed.todense()
-    best_recommendation = model.kneighbors(dense)[1][0][0]
-    strain = strains.iloc[best_recommendation]
-    output = strain.drop(['Unnamed: 0', 'name', 'ailment', 'all_text', 'lemmas']).to_dict()
-    return output
+# def predict(request_text):
+#     transformed = transformer.transform(request_text)
+#     dense = transformed.todense()
+#     best_recommendation = model.kneighbors(dense)[1][0][0]
+#     strain = strains.iloc[best_recommendation]
+#     output = strain.drop(['Unnamed: 0', 'name', 'ailment', 'all_text', 'lemmas']).to_dict()
+#     return output
 #######################################################################################################################
 
 def create_app():
@@ -51,10 +51,10 @@ def create_app():
     db = SQLAlchemy(app)
     db.init_app(app)
 
-    @app.route('/predict', methods=['POST', 'GET'])
-    def root():
-        req_data = request.get_json(force=True)
-        return predict(req_data)
+    # @app.route('/predict', methods=['POST', 'GET'])
+    # def root():
+    #     req_data = request.get_json(force=True)
+    #     return predict(req_data)
 
 
     return app
