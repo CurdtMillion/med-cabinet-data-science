ef14d255725cd3af2b1dbe2decd5156506a4da98
John Mercedes <mercedes.john@gmail.com>
2020-03-02 20:09:56 -0500

    added heroku-api + Procfile

--- API/__init__.py
+++ API/__init__.py
@@ -1,4 +1,9 @@
 """Entry point for API"""
 from .app import create_app
 
-APP = create_app()
+if __name__ == "__main__":
+    APP = create_app()
+    my_app.run(debug=True)
+
+
+# APP = create_app()
