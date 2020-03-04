dfd85a797bcee30de608dc923d293fce137f92a1
David Vollendroff <civilianemail@gmail.com>
2020-03-04 13:04:25 -0800

    sklearn version locked, app.py import

--- API/app.py
+++ API/app.py
@@ -11,45 +11,45 @@ from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 from .models import DB, Strain
 
-from API.nearest_neighbors_model import fishbarrel
-
-fishbarrel()
-
-
-####################################################
-
-import pickle
-import pandas as pd
-from sklearn.neighbors import NearestNeighbors
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-# changed from relative to to full path
-
-
-strains = pd.read_csv("https://raw.githubusercontent.com/Build-Week-Med-Cabinet-3/Data-Science/master/API/nn_model_strains.csv")
-
-transformer = TfidfVectorizer(stop_words="english", min_df=0.025, max_df=0.98, ngram_range=(1,3))
-
-dtm = transformer.fit_transform(strains['lemmas'])
-dtm = pd.DataFrame(dtm.todense(), columns=transformer.get_feature_names())
-
-model = NearestNeighbors(n_neighbors=3, algorithm='kd_tree')
-model.fit(dtm)
-
-
-
-def predict(request_text):
-    transformed = transformer.transform([request_text])
-    dense = transformed.todense()
-    recommendations = model.kneighbors(dense)[1][0]
-    output_array = []
-    for recommendation in recommendations:
-        strain = strains.iloc[recommendation]
-        output = strain.drop(['Unnamed: 0', 'name', 'ailment', 'all_text', 'lemmas']).to_dict()
-        output_array.append(output)
-    return output_array
-
-##################################################
+from API.nearest_neighbors_model import predict
+#
+# fishbarrel()
+#
+#
+# ####################################################
+#
+# import pickle
+# import pandas as pd
+# from sklearn.neighbors import NearestNeighbors
+# from sklearn.feature_extraction.text import TfidfVectorizer
+#
+# # changed from relative to to full path
+#
+#
+# strains = pd.read_csv("https://raw.githubusercontent.com/Build-Week-Med-Cabinet-3/Data-Science/master/API/nn_model_strains.csv")
+#
+# transformer = TfidfVectorizer(stop_words="english", min_df=0.025, max_df=0.98, ngram_range=(1,3))
+#
+# dtm = transformer.fit_transform(strains['lemmas'])
+# dtm = pd.DataFrame(dtm.todense(), columns=transformer.get_feature_names())
+#
+# model = NearestNeighbors(n_neighbors=3, algorithm='kd_tree')
+# model.fit(dtm)
+#
+#
+#
+# def predict(request_text):
+#     transformed = transformer.transform([request_text])
+#     dense = transformed.todense()
+#     recommendations = model.kneighbors(dense)[1][0]
+#     output_array = []
+#     for recommendation in recommendations:
+#         strain = strains.iloc[recommendation]
+#         output = strain.drop(['Unnamed: 0', 'name', 'ailment', 'all_text', 'lemmas']).to_dict()
+#         output_array.append(output)
+#     return output_array
+#
+# ##################################################
 
 
 def create_app():
