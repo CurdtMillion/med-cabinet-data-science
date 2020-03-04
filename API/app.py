5be17f506513404fe3d7ce76a40aeea14181c733
andronikmk <andronikmk@gmail.com>
2020-03-04 13:27:49 -0500

    add nn-model

--- API/app.py
+++ API/app.py
@@ -9,15 +9,43 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
-from .models import DB
+from .models import DB, Strain
 
 # import model
-from nearest_neighbors_model import predict
+#from nearest_neighbors_model import predict
+
+
+####################################################
+
+import pickle
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# changed from relative to to full path
+model = pickle.load(open("../models/nearest_neighbors_model.sav", "rb"))
+transformer = pickle.load(open("../models/transformer.sav", "rb"))
+strains = pd.read_csv("../src/data/nn_model_strains.csv")
+
+
+def predict(request_text):
+    transformed = transformer.transform([request_text])
+    dense = transformed.todense()
+    recommendations = model.kneighbors(dense)[1][0]
+    output_array = []
+    for recommendation in recommendations:
+        strain = strains.iloc[recommendation]
+        output = strain.drop(['Unnamed: 0', 'name', 'ailment', 'all_text', 'lemmas']).to_dict()
+        output_array.append(output)
+    return output_array
+
+##################################################
 
 
 def create_app():
     """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
+    # consider using config
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
