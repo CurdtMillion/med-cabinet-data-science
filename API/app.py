243a40275d53057c1ad6ef7098f79b24ec43e540
David Vollendroff <civilianemail@gmail.com>
2020-03-04 13:09:28 -0800

    removes db aspects for testing

--- API/app.py
+++ API/app.py
@@ -54,28 +54,28 @@ from API.nearest_neighbors_model import predict
 
 def create_app():
     """Create and configure an instance of the Flask application"""
-    app = Flask(__name__)
+    # app = Flask(__name__)
     # consider using config
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # load file from .env file
-    load_dotenv()
-    db_name = os.getenv("DB_NAME")
-    db_user = os.getenv("DB_USER")
-    db_password = os.getenv("DB_PASSWORD")
-    db_host = os.getenv("DB_HOST")
-
-    # establish cursor and connection
-    connection = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host)
-    print("CONNECTION:", connection)
-    cursor = connection.cursor()
-    print("CURSOR:", cursor)
-
-    # binding the instance to a very specific Flask app
-    # initialize app for use with this database setup
-    db = SQLAlchemy(app)
-    db.init_app(app)
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL")
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #
+    # # load file from .env file
+    # load_dotenv()
+    # db_name = os.getenv("DB_NAME")
+    # db_user = os.getenv("DB_USER")
+    # db_password = os.getenv("DB_PASSWORD")
+    # db_host = os.getenv("DB_HOST")
+    #
+    # # establish cursor and connection
+    # connection = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host)
+    # print("CONNECTION:", connection)
+    # cursor = connection.cursor()
+    # print("CURSOR:", cursor)
+    #
+    # # binding the instance to a very specific Flask app
+    # # initialize app for use with this database setup
+    # db = SQLAlchemy(app)
+    # db.init_app(app)
 
     # root route
     @app.route('/')
